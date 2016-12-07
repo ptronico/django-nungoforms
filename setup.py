071642a7dce202da1bb30cfb42a7e1e54eb54f01
Pedro Vasconcelos <ptronico@gmail.com>
2016-12-07 13:48:31 -0300

    Third commit.

--- setup.py
+++ setup.py
@@ -9,23 +9,27 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-nungoforms',
-    version='0.1',
-    packages=['nungoforms'],
+    version='0.1.1',
+    packages=[
+        'nungoforms',
+        'nungoforms.migrations',
+        'nungoforms.templatetags',
+    ],
     include_package_data=True,
     license='MIT',
     description='Nungoforms is a very simple Django app to super-DRY your templates.',
     long_description=README,
     url='https://github.com/ptronico/django-nungoforms',
+    download_url='https://github.com/ptronico/django-nungoforms/archive/0.1.tar.gz',
     author='Pedro Vasconcelos',
     author_email='ptronico@gmail.com',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License', # example license
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Topic :: Internet :: WWW/HTTP',
